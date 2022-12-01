da28ac15a0ea2623fb7b65506c413019ba461fa2
Jteve-Sobs <46194843+Jteve-Sobs@users.noreply.github.com>
2022-12-01 17:11:16 +0100

    First commit and solutions for day 1

---
+++ AdventOfCode0101/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            input.Add(""); // Add line at the end to add last result to list
+
+            var results = new List<int>();
+
+            var tmp = 0;
+
+            foreach (var elfCalories in input)
+            {
+                if (elfCalories == "")
+                {
+                    results.Add(tmp);
+                    tmp = 0;
+                    continue;
+                }
+
+                tmp += Convert.ToInt32(elfCalories);
+            }
+
+            // Assert for input1
+            //Assert.AreEqual(24000, results.Max(), "Result is not correct");
+
+            Console.WriteLine("Result: " + results.Max());
+        }
+    }
+}
---
+++ AdventOfCode0102/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            input.Add(""); // Add line at the end to add last result to list
+
+            var results = new List<int>();
+
+            var tmp = 0;
+
+            foreach (var elfCalories in input)
+            {
+                if (elfCalories == "")
+                {
+                    results.Add(tmp);
+                    tmp = 0;
+                    continue;
+                }
+
+                tmp += Convert.ToInt32(elfCalories);
+            }
+
+            var endresults = new List<int>() { 0, 0, 0 };
+            foreach (var result in results)
+            {
+                // Change smallest result with larger result
+                if (endresults.Min() < result)
+                {
+                    endresults.Remove(endresults.Min());
+                    endresults.Add(result);
+                }
+            }
+
+            // Assert for input1
+            Assert.AreEqual(45000, endresults.Sum(), "Result is not correct");
+
+            Console.WriteLine("Result: " + endresults.Sum());
+        }
+    }
+}
