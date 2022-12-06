70c7c4cf0028ca340a4675c2082f9e3e534b1f84
Jteve-Sobs <46194843+Jteve-Sobs@users.noreply.github.com>
2022-12-06 12:32:34 +0100

    Day 6

---
+++ AdventOfCode0601/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0601
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            var marker = 0;
+
+            var charactersUsed = new List<char>();
+
+            foreach (var character in input.First())
+            {
+                marker++;
+                charactersUsed.Add(character);
+                if (charactersUsed.Count == 4)
+                {
+                    if (charactersUsed.Distinct().Count() == 4)
+                    {
+                        break;
+                    }
+                    charactersUsed.RemoveAt(0);
+                }
+            }
+
+            // Assert for input1
+            //Assert.AreEqual(10, marker, "Result is not correct");
+
+            Console.WriteLine("Result: " + marker);
+        }
+    }
+}
---
+++ AdventOfCode0602/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0602
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            var marker = 0;
+
+            var charactersUsed = new List<char>();
+
+            foreach (var character in input.First())
+            {
+                marker++;
+                charactersUsed.Add(character);
+                if (charactersUsed.Count == 14)
+                {
+                    if (charactersUsed.Distinct().Count() == 14)
+                    {
+                        break;
+                    }
+                    charactersUsed.RemoveAt(0);
+                }
+            }
+
+            // Assert for input1
+            //Assert.AreEqual(29, marker, "Result is not correct");
+
+            Console.WriteLine("Result: " + marker);
+        }
+    }
+}
