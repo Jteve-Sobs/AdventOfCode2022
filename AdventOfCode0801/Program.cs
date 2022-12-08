bcc80f24c9c22356a98e906f9b8bae742100ec0c
Jteve-Sobs <46194843+Jteve-Sobs@users.noreply.github.com>
2022-12-08 22:35:57 +0100

    Day 8

---
+++ AdventOfCode0801/Program.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0801
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            var result = 0;
+
+            var trees = new List<List<int>>();
+
+            foreach (var line in input)
+            {
+                trees.Add(new List<int>());
+                foreach (var tree in line)
+                {
+                    trees.Last().Add(Convert.ToInt32(tree.ToString()));
+                }
+            }
+
+            // Add outer trees
+            for (int i = 0; i < trees.Count; i++)
+            {
+                for (int j = 0; j < trees.ElementAt(i).Count; j++)
+                {
+                    if (i == 0 || i == trees.Count - 1 || j == 0 || j == trees.ElementAt(i).Count - 1)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            // Evalute inner trees
+            for (int i = 1; i < trees.Count - 1; i++)
+            {
+                for (int j = 1; j < trees.ElementAt(i).Count - 1; j++)
+                {
+                    var currentTree = trees.ElementAt(i).ElementAt(j);
+
+                    // Top
+                    var counter = 0;
+                    for (int k = 0; k < i; k++)
+                    {
+                        if (trees.ElementAt(k).ElementAt(j) >= currentTree)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 0)
+                    {
+                        result++;
+                        continue;
+                    }
+
+                    // Bottom
+                    counter = 0;
+                    for (int k = i + 1; k < trees.Count; k++)
+                    {
+                        if (trees.ElementAt(k).ElementAt(j) >= currentTree)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 0)
+                    {
+                        result++;
+                        continue;
+                    }
+
+                    // Left
+                    counter = 0;
+                    for (int k = 0; k < j; k++)
+                    {
+                        if (trees.ElementAt(i).ElementAt(k) >= currentTree)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 0)
+                    {
+                        result++;
+                        continue;
+                    }
+
+                    // Right
+                    counter = 0;
+                    for (int k = j + 1; k < trees.First().Count; k++)
+                    {
+                        if (trees.ElementAt(i).ElementAt(k) >= currentTree)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 0)
+                    {
+                        result++;
+                        continue;
+                    }
+                }
+            }
+
+            // Assert for input1
+            //Assert.AreEqual(21, result, "Result is not correct");
+            
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
---
+++ AdventOfCode0802/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode0802
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("..\\..\\input1.txt").ToList();
+            var input = File.ReadAllLines("..\\..\\input2.txt").ToList();
+
+            var result = 0;
+
+            var trees = new List<List<int>>();
+
+            foreach (var line in input)
+            {
+                trees.Add(new List<int>());
+                foreach (var tree in line)
+                {
+                    trees.Last().Add(Convert.ToInt32(tree.ToString()));
+                }
+            }
+
+            // Evalute trees
+            for (int i = 1; i < trees.Count - 1; i++)
+            {
+                for (int j = 1; j < trees.ElementAt(i).Count - 1; j++)
+                {
+                    var currentTree = trees.ElementAt(i).ElementAt(j);
+
+                    var partResults = new List<int>();
+
+                    var tmpResult = 0;
+                    // Top
+                    for (int k = i - 1; k >= 0; k--)
+                    {
+                        tmpResult++;
+                        if (trees.ElementAt(k).ElementAt(j) >= currentTree)
+                        {
+                            break;
+                        }
+                    }
+                    partResults.Add(tmpResult);
+
+                    // Bottom
+                    tmpResult = 0;
+                    for (int k = i + 1; k < trees.Count; k++)
+                    {
+                        tmpResult++;
+                        if (trees.ElementAt(k).ElementAt(j) >= currentTree)
+                        {
+                            break;
+                        }
+                    }
+                    partResults.Add(tmpResult);
+
+                    // Left
+                    tmpResult = 0;
+                    for (int k = j - 1; k >= 0; k--)
+                    {
+                        tmpResult++;
+                        if (trees.ElementAt(i).ElementAt(k) >= currentTree)
+                        {
+                            break;
+                        }
+                    }
+                    partResults.Add(tmpResult);
+
+                    // Right
+                    tmpResult = 0;
+                    for (int k = j + 1; k < trees.First().Count; k++)
+                    {
+                        tmpResult++;
+                        if (trees.ElementAt(i).ElementAt(k) >= currentTree)
+                        {
+                            break;
+                        }
+                    }
+                    partResults.Add(tmpResult);
+
+                    // Calculate and evalute result
+                    var resultCandidate = 1;
+                    foreach (var item in partResults)
+                    {
+                        resultCandidate *= item;
+                    }
+
+                    if (resultCandidate > result)
+                    {
+                        result = resultCandidate;
+                    }
+                }
+            }
+
+            // Assert for input1
+            //Assert.AreEqual(8, result, "Result is not correct");
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
